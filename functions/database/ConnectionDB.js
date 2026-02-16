90c45103cf1f900df1bce309a2d6e4657837a396
RichiGM <garnicamejiaricardo@gmail.com>
2026-02-16 11:07:03 -0600

    nuevo push

--- functions/database/ConnectionDB.js
+++ functions/database/ConnectionDB.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       dbName: process.env.DB_NAME
     });
+
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
